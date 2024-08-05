d13b221e5a6a13c76ac6b2016d13cfec794d8a36
yoshago <yoshago@gmail.com>
2024-08-05 06:36:23 -0400

    feat: 🎸 adding a workerPool and a docker

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,15 +1,24 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/yoshago/order-processing-system/internal/models"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func InitDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=shaag password=752PGpanor dbname=orderdb port=5433 sslmode=disable TimeZone=Asia/Jerusalem"
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jerusalem", dbHost, dbUser, dbPassword, dbName, dbPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
